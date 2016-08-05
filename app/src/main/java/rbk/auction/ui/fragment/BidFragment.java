e237bd0cf4e5b052f625c24631f13ebe6b0aa518
Ravi <a@a.in>
2016-08-05 23:12:47 +0530

    added  fragment

--- app/src/main/java/rbk/auction/ui/fragment/BidFragment.java
+++ app/src/main/java/rbk/auction/ui/fragment/BidFragment.java
@@ -13,12 +13,12 @@ import rbk.auction.R;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link AuctionFragment.OnFragmentInteractionListener} interface
+ * {@link BidFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link AuctionFragment#newInstance} factory method to
+ * Use the {@link BidFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class AuctionFragment extends Fragment {
+public class BidFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -30,7 +30,7 @@ public class AuctionFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    public AuctionFragment() {
+    public BidFragment() {
         // Required empty public constructor
     }
 
@@ -40,11 +40,11 @@ public class AuctionFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment AuctionFragment.
+     * @return A new instance of fragment BidFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static AuctionFragment newInstance(String param1, String param2) {
-        AuctionFragment fragment = new AuctionFragment();
+    public static BidFragment newInstance(String param1, String param2) {
+        BidFragment fragment = new BidFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -65,7 +65,7 @@ public class AuctionFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_auction, container, false);
+        return inflater.inflate(R.layout.fragment_bid, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -97,7 +97,7 @@ public class AuctionFragment extends Fragment {
      * fragment to allow an interaction in this fragment to be communicated
      * to the activity and potentially other fragments contained in that
      * activity.
-     * <p>
+     * <p/>
      * See the Android Training lesson <a href=
      * "http://developer.android.com/training/basics/fragments/communicating.html"
      * >Communicating with Other Fragments</a> for more information.
